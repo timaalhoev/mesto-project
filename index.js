49e5829edd3abab2d24d8eee16f969cd1419a828
Timur Alhoev <timuralhoev@yandex.ru>
2021-12-14 01:50:15 +0300

    fix after review

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const edit = document.querySelector('.profile__edit-button')
 const plus = document.querySelector('.profile__button')
+const title = document.querySelector('.profile__title')
+const subtitle = document.querySelector('.profile__subtitle')
+/* нахожу элементы попапа popup_user */
 const popupUser = document.querySelector('.popup_user')
-const popupPlace = document.querySelector('.popup_place')
-const popupImage = document.querySelector('.popup_image')
+const formUser = document.querySelector('.form__user')
+const nameInput = document.querySelector('.field__name')
+const jobInput = document.querySelector('.field__job')
 const closePopupUser = document.querySelector('.popup_user .popup__close')
+/* нахожу элементы попапа popup_place */
+const popupPlace = document.querySelector('.popup_place')
+const formPlace = document.querySelector('.form__place')
+const labelInput = document.querySelector('.field__label')
+const imageInput = document.querySelector('.field__image')
 const closePopupPlace = document.querySelector('.popup_place .popup__close')
+/* нахожу элементы попапа popup_image */
+const popupImage = document.querySelector('.popup_image')
+const image = document.querySelector('.popup_image .popup__picture')
+const closePopupImage = document.querySelector('.popup_image .popup__close')
+const popupImageTitle = document.querySelector('.popup_image .popup__text')
+/* нахожу template карточки места */
+const templateLocation = document.getElementById('template__location')
 
 /* открываем и закрываем попап "Редактировать юзера" */
 edit.addEventListener('click', () => openPopup(popupUser))
@@ -15,175 +31,100 @@ plus.addEventListener('click', () => openPopup(popupPlace))
 closePopupPlace.addEventListener('click', () => closePopup(popupPlace))
 
 function openPopup(el) {
-    el.classList.add('popup_opened')
+  el.classList.add('popup_opened')
 }
 
 function closePopup(el) {
-    el.classList.remove('popup_opened')
+  el.classList.remove('popup_opened')
 }
 
-/* нахожу элементы формы */
-const formUser = document.querySelector('.form__user')
-const nameInput = document.querySelector('.field__name')
-const jobInput = document.querySelector('.field__job')
-const title = document.querySelector('.profile__title')
-const subtitle = document.querySelector('.profile__subtitle')
-
 // навешиваю обработчик события 'submit'
-formUser.addEventListener('submit', formUserSubmitHandler)
+formUser.addEventListener('submit', submitFormUserHandler)
 
 // описываю функцию-обработчик
-function formUserSubmitHandler (event) {
-    event.preventDefault()
+function submitFormUserHandler (event) {
+  event.preventDefault()
 
-    // достаю значения из инпутов формы
-    const name = nameInput.value
-    const job = jobInput.value
+  // достаю значения из инпутов формы
+  const name = nameInput.value
+  const job = jobInput.value
 
-    title.textContent = name // задаю новое имя
-    subtitle.textContent = job // задаю новую работу
+  title.textContent = name // задаю новое имя
+  subtitle.textContent = job // задаю новую работу
 
-    closePopup(popupUser) // вызываю функцию закрывающую popup
+  closePopup(popupUser) // вызываю функцию закрывающую popup
 }
 
 const initialCards = [
-    {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
 ];
 
 const locationRow = document.querySelector('.locations__row')
 
 initialCards.forEach(item => {
-    const location = document.createElement('div')
-    location.classList.add('location')
-
-    const locationBlock = document.createElement('div')
-    locationBlock.classList.add('location__block')
-
-    const location__title = document.createElement('h2')
-    location__title.classList.add('location__title')
-    location__title.textContent = item.name
-
-    const locationLike = document.createElement('button')
-    locationLike.classList.add('location__like')
-
-    locationLike.addEventListener('click', function () {
-        locationLike.classList.toggle('active')
-    })
-
-    let icon = document.createElement('img')
-    icon.setAttribute('src', './images/delite.svg')
-    icon.classList.add('location__delete')
-
-    icon.addEventListener('click', function () {
-        icon.parentNode.remove()
-    })
-
-    const locationImage = document.createElement('img')
-    locationImage.classList.add('location__images')
-    locationImage.setAttribute('src', item.link)
-
-    locationImage.addEventListener('click', function () {
-      popupImage.innerHTML = ''
-      popupImage.classList.add('popup_opened')
-
-      const buttonClose = document.createElement('button')
-      buttonClose.classList.add('popup__close')
-      buttonClose.addEventListener('click', () => closePopup(popupImage))
-
-      const image = document.createElement('img')
-      image.classList.add('popup__picture')
-      image.setAttribute('src', item.link)
-
-      const text = document.createElement('p')
-      text.classList.add('popup__text')
-      text.textContent = item.name
-
-      const container = document.createElement('div')
-      container.classList.add('popup__container')
-      container.classList.add('popup_big')
-
-      container.appendChild(buttonClose)
-      container.appendChild(image)
-      container.appendChild(text)
-
-      popupImage.appendChild(container)
-    })
-
-    locationBlock.appendChild(location__title)
-    locationBlock.appendChild(locationLike)
-
-
-    location.appendChild(locationImage)
-    location.appendChild(locationBlock)
-    location.appendChild(icon)
-
-    locationRow.appendChild(location)
+  createCard(item)
 })
 
-const labelInput = document.querySelector('.field__label')
-const imageInput = document.querySelector('.field__image')
-const formPlace = document.querySelector('.form__place')
+formPlace.addEventListener('submit', submitFormPlaceHandler)
 
-formPlace.addEventListener('submit', formPlaceSubmitHandler)
+function submitFormPlaceHandler(event) {
+  event.preventDefault()
 
-function formPlaceSubmitHandler(event) {
-    event.preventDefault()
+  createCard({
+    name: labelInput.value,
+    link: imageInput.value
+  })
 
-    const assign = labelInput.value
-    const link  = imageInput.value
+  closePopup(popupPlace)
+}
 
-    const location = document.createElement('div')
-    location.classList.add('location')
-    const locationBlock = document.createElement('div')
-    locationBlock.classList.add('location__block')
+function createCard(item) {
+  const location = templateLocation.content
+  const locationCard = location.cloneNode(true)
 
-    const locationTitle = document.createElement('h3')
-    locationTitle.classList.add('location__title')
-    locationTitle.textContent = assign
+  const locationImage = locationCard.querySelector('.location__images')
+  locationImage.src = item.link
+  locationImage.addEventListener('click', function () {
+    openPopup(popupImage)
 
-    const locationLike = document.createElement('button')
-    locationLike.classList.add('location__like')
+    closePopupImage.addEventListener('click', () => closePopup(popupImage))
+    image.setAttribute('src', item.link)
+    popupImageTitle.textContent = item.name
+  })
 
-    locationBlock.appendChild(locationTitle)
-    locationBlock.appendChild(locationLike)
+  locationCard.querySelector('.location__title').textContent = item.name
 
-    const locationDelete = document.createElement('img')
-    locationDelete.setAttribute('src', './images/delite.svg')
-    locationDelete.classList.add('location__delete')
+  locationCard.querySelector('.location__like').addEventListener('click', function () {
+    locationLike.classList.toggle('active')
+  })
 
-    const locationImage = document.createElement('img')
-    locationImage.classList.add('location__images')
-    locationImage.setAttribute('src', link)
+  const icon = locationCard.querySelector('.location__delete')
+  icon.addEventListener('click', function () {
+    icon.parentNode.remove()
+  })
 
-
-    location.appendChild(locationImage)
-    location.appendChild(locationBlock)
-    location.appendChild(locationDelete)
-
-    locationRow.prepend(location)
-
-    closePopup(popupPlace)
+  locationRow.prepend(locationCard)
 }
